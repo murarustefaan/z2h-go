c2e7d0caed8d3530114851e4b0c8256589298820
Ștefan MURARU <muraru.stefaan@gmail.com>
2023-11-04 20:41:59 +0200

    feat(square): full pipeline with param input

--- square.go
+++ square.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 type Combo struct {
@@ -9,12 +11,25 @@ type Combo struct {
 	squared  int
 }
 
-func generate(numbers []int) <-chan Combo {
+func generate(count int) <-chan int {
+	output := make(chan int)
+
+	go func() {
+		defer close(output)
+		for i := 1; i <= count; i++ {
+			output <- i
+		}
+	}()
+
+	return output
+}
+
+func create(input <-chan int) <-chan Combo {
 	output := make(chan Combo)
 
 	go func() {
 		defer close(output)
-		for _, v := range numbers {
+		for v := range input {
 			combo := Combo{original: v}
 			output <- combo
 		}
@@ -39,12 +54,24 @@ func square(input <-chan Combo) <-chan Combo {
 }
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5}
+	count := 100
 
-	input := generate(numbers)
-	output := square(input)
+	fmt.Println(os.Args)
+	if len(os.Args) >= 2 {
+		parsed, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Printf("Invalid argument, defaulting to %d\n", count)
+		}
+		if parsed > 0 {
+			count = parsed
+		}
+	}
 
-	for v := range output {
-		fmt.Printf("%d squared is %d\n", v.original, v.squared)
+	input := generate(count)
+	mapped := create(input)
+	output := square(mapped)
+
+	for val := range output {
+		fmt.Printf("%d squared is %d\n", val.original, val.squared)
 	}
 }
